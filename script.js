83729c2838f79c01e332c899e8f8c13e8ccea4c1
Sushant Badangod <sushant.bs@gmail.com>
2019-06-22 14:03:46 +0530

    Updated for Aneesh

--- script.js
+++ script.js
@@ -1,50 +1,27 @@
 // Game logic
 
-let player = 1;
-let turnsPlayed = new Map();
+// This is the player whose turn it is to play
+var playerToPlay = 1;
+
+// This is a data structure to store all the turns that have been played so far
+var turnsPlayed = new Map();
 
 function checkGameState() {
-    // game state logic
+  // check the game state logic to determine if the game is over
 }
 
 function showUserMessage(text) {
-    if (text) {
-        document.getElementById("user-message").style.display = "block";
-        document.getElementById("user-message").innerHTML = text;
-    } else {
-        document.getElementById("user-message").style.display = "none";
-    }
+  // show messages to the user
 }
 
 function handleTileClick(e) {
-    let tileElement = e.target,
-        tileId = tileElement.getAttribute("data-index");
-
-    if (turnsPlayed.has(tileId)) {
-        showUserMessage("tile already occupied!");
-        return;
-    }
-
-    showUserMessage("");
-
-    if (player === 1) {
-        tileElement.innerHTML = `<span class="xtile">X</span>`;
-        turnsPlayed.set(tileId, "X");
-    } else {
-        tileElement.innerHTML = `<span class="otile">O</span>`;
-        turnsPlayed.set(tileId, "O");
-    }
-    player = (player + 1) % 2;
-    checkGameState();
+  // this is the function that should be invoked to handle tile click
 }
 
 function initialize() {
-
-    let tdElements = document.getElementsByTagName("td");
-    tdElements = Array.prototype.slice.call(tdElements);
-    tdElements.forEach(element => element.addEventListener("click", e => handleTileClick(e)));
+  // initialize the grid to handle the correct inputs
 }
 
-window.onload = function () {
-    initialize();
-}
+window.onload = function() {
+  initialize();
+};
