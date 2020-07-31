9fd86478c5b8d4de3be4e32ce4a2eb2f9aa7913e
Sushant Badangod <sushant.bs@gmail.com>
2020-07-31 08:51:51 +0200

    Added package.json to run tsc from

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tictactoe
 A simple tic-tac-toe test
+
+## How to run
+1. Clone the repo and `cd` into the project root
+1. `yarn`
+1. `yarn start --watch `
+1. open index.html in the browser
--- script.ts
+++ script.ts
@@ -3,8 +3,16 @@
 // This is the player whose turn it is to play
 let playerToPlay = 1;
 
+type Turn = {
+  // define the type here
+}
+
+type TurnsPlayed = {
+  // define the data structure you need to track the types here
+}
+
 // This is a data structure to store all the turns that have been played so far
-const turnsPlayed = new Map();
+let turnsPlayed: TurnsPlayed;
 
 function checkGameState() {
   // check the game state logic to determine if the game is over
